bd7600455d7e593dddd56992fb630589b3e1c314
Mayank bisht <mayankbisht8532@gmail.com>
2025-07-17 22:05:44 +0530

    splash screen

--- app/src/main/java/com/example/mbchats/splash.java
+++ app/src/main/java/com/example/mbchats/splash.java
@@ -1,6 +1,8 @@
 package com.example.mbchats;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,14 @@ public class splash extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(splash.this, MainActivity.class);
+
+                startActivity(intent);
+                finish(); // Close current activity
+            }
+        }, 3000);
     }
 }
