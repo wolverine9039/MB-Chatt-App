beb38d748bf6e3fb7666ab0d15ef08ba50be5c47
Mayank bisht <mayankbisht8532@gmail.com>
2025-07-18 16:06:37 +0530

    logi screen

--- app/src/main/java/com/example/mbchats/login.java
+++ app/src/main/java/com/example/mbchats/login.java
@@ -1,6 +1,5 @@
 package com.example.mbchats;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,28 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.firebase.auth.FirebaseAuth;
+public class login extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
-FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
-
         });
-
-        auth=FirebaseAuth.getInstance();
-        if(auth.getCurrentUser()==null)
-        {
-            Intent i=new Intent(MainActivity.this, login.class);
-            startActivity(i);
-        }
     }
 }
