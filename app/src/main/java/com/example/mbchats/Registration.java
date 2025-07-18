8da6e97fb1d6c1cbfa8dd7b98d52f9243c6193ca
Mayank bisht <mayankbisht8532@gmail.com>
2025-07-18 23:19:32 +0530

    rgistration page

--- app/src/main/java/com/example/mbchats/Registration.java
+++ app/src/main/java/com/example/mbchats/Registration.java
@@ -1,8 +1,6 @@
 package com.example.mbchats;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,23 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class splash extends AppCompatActivity {
+public class Registration extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_registration);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        new Handler().postDelayed(() -> {
-            Intent intent=new Intent(splash.this, Registration.class);
-
-            startActivity(intent);
-            finish(); // Close current activity
-        }, 3000);
     }
 }
